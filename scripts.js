b63aad705a70f660d05dc2b778be3f54962b83ab
SIDVJSINGH <sidvjsingh@gmail.com>
2023-07-20 18:33:50 +0530

    Score added

--- scripts.js
+++ scripts.js
@@ -13,10 +13,22 @@ function jump() {
 
         setTimeout(function() {
             dino.classList.remove("jump");
-        }, 500);
+        }, 450);
         score += 2;
     }
 }
+
+var score_now = setInterval(function() {
+    var now = score;
+    document.getElementById("score").innerHTML = now;
+}, 500);
+
+var game_over = setInterval(function() {
+    if (score >= 100) {
+        alert("Level Completed " + score);
+        score = 0;
+    }
+}, 2500);
 
 let isAlive = setInterval(function() {
     // get current dino Y position
@@ -33,7 +45,7 @@ let isAlive = setInterval(function() {
     if (cactusLeft <= 65 && cactusLeft > -20 && dinoTop >= 0) {
         // collision
         // window.console.log("collision");
-        alert("Game Over! Your Score is: " + score);
+        // alert("Game Over! Your Score is: " + score);
         score = 0;
     }
 }, 10);
