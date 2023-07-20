1ea8fe5441633823aac57e08c336d472b5e20c44
SIDVJSINGH <sidvjsingh@gmail.com>
2023-07-20 17:11:24 +0530

    no changes

--- scripts.js
+++ scripts.js
@@ -8,36 +8,36 @@ const cactus = document.getElementById("cactus");
 let score = 0;
 
 function jump() {
-  if (dino.classList != "jump") {
-    dino.classList.add("jump");
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
 
-    setTimeout(function () {
-      dino.classList.remove("jump");
-    }, 500);
-    score += 2;
-  }
+        setTimeout(function() {
+            dino.classList.remove("jump");
+        }, 500);
+        score += 2;
+    }
 }
 
-let isAlive = setInterval(function () {
-  // get current dino Y position
-  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-  // window.console.log(dinoTop);
+let isAlive = setInterval(function() {
+    // get current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    // window.console.log(dinoTop);
 
-  // get current cactus X position
-  let cactusLeft = parseInt(
-    window.getComputedStyle(cactus).getPropertyValue("left")
-  );
-  // window.console.log(cactusLeft);
+    // get current cactus X position
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+    // window.console.log(cactusLeft);
 
-  // detect collision
-  if (cactusLeft <= 65 && cactusLeft > -20 && dinoTop >= 0) {
-    // collision
-    // window.console.log("collision");
-    alert("Game Over! Your Score is: " + score);
-    score = 0;
-  }
+    // detect collision
+    if (cactusLeft <= 65 && cactusLeft > -20 && dinoTop >= 0) {
+        // collision
+        // window.console.log("collision");
+        alert("Game Over! Your Score is: " + score);
+        score = 0;
+    }
 }, 10);
 
-document.addEventListener("keydown", function (event) {
-  jump();
+document.addEventListener("keydown", function(event) {
+    jump();
 });
